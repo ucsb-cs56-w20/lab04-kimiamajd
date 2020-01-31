c380a9c166732bb0b0f8bc436a362fbf8688fc51
“Kimia <kimiamajd@umail.ucsb.edu>
2020-01-30 16:28:09 -0800

    KM - updated javadoc comments and add javadoc

--- src/main/java/edu/ucsb/cs56/ratcalc/model/Rational.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/model/Rational.java
@@ -25,12 +25,17 @@ public class Rational {
 	else
 	    return gcd(b%a, a);
     }
-    
+    /**Constructor for rational class */
     public Rational() {
 	this.num = 1;
 	this.denom = 1;
     }
-
+	/**
+	 * parameterized constructor for rational class
+	 * @param num numerator
+	 * @param denom denominator
+	 * 
+	 */
     public Rational(int num, int denom) {
 	if (denom== 0) {
 	    throw new IllegalArgumentException("denominator may not be zero");
@@ -43,7 +48,9 @@ public class Rational {
 	    this.denom /= gcd;
 	}
     }
-
+	/**
+	 * @return the rational number as a string 
+ 	*/
     public String toString() {
 	if (denom < 0) {
 		denom = -1 * denom;
@@ -53,28 +60,51 @@ public class Rational {
 	    return "" + num;
 	return num + "/" + denom;
     }
-
-    public int getNumerator() { return this.num; }
+	/**
+ 	* 
+ 	* @return the numerator
+ 	*/
+	public int getNumerator() { return this.num; }
+ 
+	/**
+	 * 
+	 * @return the denominator
+	 */
     public int getDenominator() { return this.denom; }
-
+	/**
+	 * 
+	 * @param r a rational number 
+	 * @return product of this and r
+	 */
     public Rational times(Rational r) {
 	return new Rational(this.num * r.num,
 			    this.denom * r.denom);
     }
-
+	
+	/**
+	 * 
+	 * @param a a rational number
+	 * @param b another rational number
+	 * @return product of rational numbers a and b
+	 */
     public static Rational product(Rational a, Rational b) {
 	return new Rational(a.num * b.num,
 			    a.denom * b.denom);
     }
+	
 	/**
-	Returns least common multiple (always positive) of a and b.
+	 * 
+	 * @param a an integer
+	 * @param b an integer
+	 * @return least common multiple (always positive) of integers a and b.
 	 */
 	public static int lcm(int a, int b){
 		return Math.abs(a*b/gcd(a,b));
 	}
-    
+	
 	/**
-	Returns the sum of this number and r
+	 * @return the sum of this and rational number r 
+	 * @param r a rational number
 	 */
 	public Rational plus(Rational r){
 		int lcm = Rational.lcm(this.denom,r.denom);
@@ -83,25 +113,32 @@ public class Rational {
 		int rNum = r.num*(lcm/r.denom);
 		return new Rational(thisNum+rNum,lcm);
 	}
+
 	/**
-	Returns the sum of a and b
+	 * @return the sum of rational numbers a and b
+	 * @param a a rational number 
+	 * @param b another rational number
 	 */
 	public static Rational sum(Rational a, Rational b){
 		return a.plus(b);
 	}
 
 	/**
-	Returns this number minus r */
-
+	 * 
+	 * @param r the number to be subtracted by
+	 * @return this minus r
+	 */
 	public Rational minus(Rational r) {
 		Rational negativeR = new Rational(-1*r.num, r.denom); 
 		return this.plus(negativeR);
 	}
 
 	/**
-	Returns a minus b
+	 * 
+	 * @param a Rational number we are subtracting from
+	 * @param b Rational number we are subtracting by
+	 * @return a minus b
 	 */
-	
 	 public static Rational difference (Rational a, Rational b) {
 		return a.minus(b);
 	}
@@ -109,7 +146,12 @@ public class Rational {
 	/**
 	Returns the reciprocal of rational number. If numerator is 0, throws ArithmeticException
 	 */
-
+/**
+ * 
+ * @return the reciprocal of this rational number. 
+ * Throws ArithmeticException when numerator is 0
+ * @throws ArithmeticException
+ */
 	 public Rational reciprocalOf(){
 		 if (this.num == 0) {
 			 throw new ArithmeticException("Numerator may not be 0");
@@ -117,19 +159,22 @@ public class Rational {
 		return new Rational (this.denom, this.num);
 	 }
 
-	/**
-	returns this number divided by r
- 	*/
-
+	
+	 /**
+	  * 
+	  * @param r the divisor
+	  * @return this number divided by r
+	  */
 	public Rational dividedBy (Rational r) {
 		return this.times(r.reciprocalOf());
 	}
 
 
 	/**
-	returns a divided by b
+	 * @return a divided by b
+	 * @param a the dividend
+	 * @param b the divisor
 	 */
-
 	 public static Rational quotient(Rational a, Rational b) {
 		 return a.dividedBy(b);
 	 }
